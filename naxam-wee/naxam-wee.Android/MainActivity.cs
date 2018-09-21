bd3950ae45f15da441e15bb82b60ea420a582fc2
Kevin Miller <scoundrel@gmail.com>
2018-09-21 15:48:53 -0500

    Initial commit

---
+++ naxam-wee/naxam-wee.Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using naxamwee;
+
+namespace naxam_wee.Droid
+{
+    [Activity(Label = "naxam_wee", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Com.Mapbox.Mapboxsdk.Mapbox.GetInstance(this, MapboxConfig.AccessToken);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ naxam-wee/naxam-wee/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Naxam.Controls.Mapbox.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace naxamwee
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            On<iOS>().SetUseSafeArea(true);
+            var viewModel = new MainPageViewModel();
+            BindingContext = viewModel;
+            InitializeComponent();
+
+//            viewModel.UpdateViewPortAction(new Position(30, -110), 11, null, true,
+//                () => { System.Diagnostics.Debug.WriteLine("Map Moved"); });
+        }
+    }
+
+    public static class MapboxConfig
+    {
+        public static string AccessToken { get; } = "sk.eyJ1IjoibmF4YW10ZXN0IiwiYSI6ImNqNWtpb2d1ZzJpMngyd3J5ZnB2Y2JhYmQifQ.LEvGqQkAqM4MO3ZtGbQrdw";
+        public static string DefaultStyle { get; } = "mapbox://styles/kevinactualize/cjlwdwb6b3hsj2sp7baxo4j19";
+    }
+
+    public class MainPageViewModel
+    {
+        public string Message => "Do you see a map?";
+
+        public double ZoomLevel { get; set; } = 11;
+        public Position Center { get; set; } = new Position(25, -100);
+        public MapStyle MapStyle { get; set; } = new MapStyle(MapboxConfig.DefaultStyle);
+        public Action<Position, double?, double?, bool, Action> UpdateViewPortAction { get; set; }
+    }
+}
